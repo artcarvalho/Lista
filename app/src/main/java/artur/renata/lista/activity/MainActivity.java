e58ed44c64df7f5a0c38198275f54251ac8ace49
artcarvalho_ <arturcarvalho05@gmail.com>
2024-04-16 14:41:01 -0300

    commit

--- app/src/main/java/artur/renata/lista/activity/MainActivity.java
+++ app/src/main/java/artur/renata/lista/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package artur.renata.lista;
+package artur.renata.lista.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import artur.renata.lista.R;
 
 public class MainActivity extends AppCompatActivity {
 

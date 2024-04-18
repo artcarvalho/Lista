0ba61ca8bc99a1b4ddf372fa75af7d5d571f2fcd
artcarvalho_ <arturcarvalho05@gmail.com>
2024-04-18 14:42:28 -0300

    commit-com defeitos

---
+++ app/src/main/java/artur/renata/lista/adapter/MyViewHolder.java
@@ -0,0 +1,11 @@
+package artur.renata.lista.adapter;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+
+public class MyViewHolder {
+    public MyViewHolder(@NonNull View itemView){
+        super(itemView)
+    }
+}
---
+++ app/src/main/java/artur/renata/lista/model/MyItem.java
@@ -0,0 +1,12 @@
+package artur.renata.lista.model;
+
+import android.net.Uri;
+import android.util.EventLogTags;
+
+public class MyItem {
+    public Uri photo;
+    public String title;
+    public String description;
+
+
+}
